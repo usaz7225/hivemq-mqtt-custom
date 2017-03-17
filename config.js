996d2a3165ebf45a3c099fa436dce12bd0b62868
Stéphane Allegro <stephane.allegro@orange.com>
2017-03-17 17:44:36 +0100

    using environment variable for username and password

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
 websocketserver = 'liveobjects.orange-business.com';
-websocketport = 1883;
-clientusername = 'json+device';
-clientpassword = 'd7311968693e4e48b1b614397b6a6d2d';
+websocketport = 443;
